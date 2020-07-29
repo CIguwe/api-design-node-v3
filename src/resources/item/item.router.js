32fb3f496141fe0599e7d6a915a80a5a3e8b3c10
Chidera Iguwe <iguwec@gmail.com>
2020-07-29 07:50:12 -0400

    Solution

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,18 @@
 import { Router } from 'express'
 
 const router = Router()
+const mock = (req, res) => {
+  console.log(req.body)
+}
+router
+  .route('/')
+  .get(mock)
+  .post(mock)
 
+router
+  .route('/:id')
+  .get(mock)
+  .post(mock)
+  .put(mock)
+  .delete(mock)
 export default router
