16774f78321fbfc78ddae430d8557e5ffcc9957d
Chidera Iguwe <iguwec@gmail.com>
2020-08-05 10:42:37 -0400

    Update item.model.js

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,35 @@
-import mongoose from 'mongoose'
+import mongoose, { Schema, mongo } from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxlength: 50,
+      required: true,
+      trim: true
+    },
+    status: {
+      type: String,
+      default: 'active',
+      enum: ['active', 'complete', 'pastdue'],
+      required: true
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'user'
+    },
+    list: {
+      ref: 'list',
+      required: true,
+      type: mongoose.Types.ObjectId
+    }
+  },
+  { timestamps: true }
+)
+
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
+
 export const Item = mongoose.model('item', itemSchema)
